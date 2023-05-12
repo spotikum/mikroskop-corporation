f0f39ba156caa365147b99c2e537acb6d0b59c38
Indra Wahyu <wicaksana0701@gmail.com>
2023-05-13 04:47:16 +0800

    test insert data but have error

--- content/post.php
+++ content/post.php
@@ -1,12 +1,22 @@
 <?php
 	session_start();
-	require '../auth/config.php';
-
-	$content = $_GET['content'];
+	// $db = new PDO ('sqlite:../database.db');
+	
 	$uid = $_SESSION['uid'];
+	$content = $_GET['content'];
 
-	$query = "insert into content values('$uid', '$content', NOW())";
-	mysqli_query($koneksi, $query);
+	// Connect to the database
+	$db = new PDO('sqlite:../database.db');
+
+	// Prepare the INSERT statement
+	$stmt = $db->prepare('INSERT INTO posts (uid, content) VALUES (?, ?);');
+
+	// Bind the values
+	$stmt->bindValue(1, 1);
+	$stmt->bindValue(2, 'test');
+
+	// Execute the statement
+	$stmt->execute();
 
      header("Location: ../index.php");
 ?>
--- test-sqlite.php
+++ test-sqlite.php
@@ -1,34 +1,11 @@
 <?php
-   // try {
-   //    //open the database
-   //    $db = new PDO('sqlite:database.db');
-
-   //    //insert some data
-   //    $db->exec("select username, content, date from posts inner join user on user.id = posts.uid order by date desc");
-
-   //    echo $db;
-
-   //    //close the database connection
-   //    $db = null;
-   // } catch(PDOException $e) {
-   //    print 'Exception : '.$e->getMessage();
-   // }
-?>
-
-<?php
-// Connect to the database
+// Create a new PDO object
 $db = new PDO('sqlite:database.db');
 
-// Prepare a SELECT statement
-$stmt = $db->prepare('select username, content, date from posts inner join user on user.id = posts.uid order by date desc');
-
-// Execute the SELECT statement
-$stmt->execute();
-
-// Fetch the results
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-foreach ($results as $row) {
-   echo $row['username'] . ' ' . $row['content'] . ' ' . $row['date'] . "\n";
+try {
+     $db->exec("insert into posts(uid, content) values(1,'hai hai lagi')");
+} catch (PDOException $e) {
+     echo $e->getMessage();
 }
+
 ?>
