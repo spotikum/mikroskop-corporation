911832298bbba8eae33df621403423164e270242
Indra Wahyu <wicaksana0701@gmail.com>
2023-05-13 08:45:54 +0800

    fix change db from mysql to sqlite

--- content/post.php
+++ content/post.php
@@ -1,6 +1,5 @@
 <?php
 	session_start();
-	// $db = new PDO ('sqlite:../database.db');
 	
 	$uid = $_SESSION['uid'];
 	$content = $_GET['content'];
@@ -8,15 +7,11 @@
 	// Connect to the database
 	$db = new PDO('sqlite:../database.db');
 
-	// Prepare the INSERT statement
-	$stmt = $db->prepare('INSERT INTO posts (uid, content) VALUES (?, ?);');
-
-	// Bind the values
-	$stmt->bindValue(1, 1);
-	$stmt->bindValue(2, 'test');
-
-	// Execute the statement
-	$stmt->execute();
+	try {
+		$db->exec("insert into posts(uid, content) values($uid,'$content')");
+	} catch (PDOException $e) {
+		echo $e->getMessage();
+	}
 
      header("Location: ../index.php");
 ?>
